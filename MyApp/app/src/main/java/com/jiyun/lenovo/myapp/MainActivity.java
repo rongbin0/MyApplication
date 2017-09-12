d448fd35c26cfaeb728b1c624e9887c67270f40d
liurongb <1270969067@qq.com>
2017-09-12 14:14:55 +0800

    第二场第一镜

---
+++ MyApp/app/src/main/java/com/jiyun/lenovo/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jiyun.lenovo.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "心心相印,印贼做父,父相伤害,害人害己");
+    }
+}
